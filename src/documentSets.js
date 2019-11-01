955cc2e3827d1c4d255e260499ec611f7ce56abe
guillaumedsde <guillaume@desusanne.com>
2019-11-01 19:42:38 +0000

    feat: diplay document sets

--- src/documentSets.js
+++ src/documentSets.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, setState } from "react";
 
 import DocumentSet from "./documentSet";
-import { ReactSetApi } from "./ReactApiClient";
+var RestApiForPredictiveAnaltyics = require("rest_api_for_predictive_analtyics");
 
 export default function DocumentSets() {
-  var documentSets = [
-    "first document set name",
-    "the second document set name"
-  ];
+  const [documentSets, setDocumentSets] = useState([]);
 
-  var api = new ReactSetApi();
+  var api = new RestApiForPredictiveAnaltyics.SetApi();
 
-  useEffect(() => {
-    api.documentsetGet().then(({ apiDocumentSets }) => {
-      console.log(apiDocumentSets);
-    }, console.error);
-  });
+  useEffect(
+    () => {
+      api.getSets().then(apiSets => {
+        console.log(apiSets.documentSets);
+        setDocumentSets(apiSets.documentSets);
+      }, console.error);
+    },
+    [] //dependencies
+  );
 
   return (
     <div>
-      {documentSets.map(setName => (
-        <DocumentSet key={setName} name={setName} />
+      {documentSets.map(set => (
+        <DocumentSet key={set.name} name={set.name} />
       ))}
     </div>
   );
