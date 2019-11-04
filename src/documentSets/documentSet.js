c7070f49c0888d0b84534df461e4e499cd8d7b02
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2019-11-04 19:10:00 +0000

    feat: document display and fixed NaN for id

--- src/documentSets/documentSet.js
+++ src/documentSets/documentSet.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+import { Link } from "@reach/router";
+
 import { SetApi } from "@harpocrates/api-client";
 
 import DocumentUploadForm from "../documents/documentUploadForm";
@@ -17,16 +19,36 @@ export default function DocumentSet(props) {
     },
     [] //dependencies
   );
-
-  return (
-    <div>
-      <ul>
-        {documents.map(document => (
-          // FIXME document name is "NaN"
-          <li key={document.name}>{document.name}</li>
-        ))}
-      </ul>
-      <DocumentUploadForm documentSet={props.documentSetName} />
-    </div>
-  );
+  // tell if no document Sets
+  if (documents.length == 0) {
+    return (
+      <div>
+        <p>No Documents</p>
+        <DocumentUploadForm documentSet={props.documentSetName} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul>
+          {documents.map(document => (
+            // FIXME document name is "NaN"
+            <li key={document.documentId}>
+              <Link
+                to={
+                  "/documentSet/" +
+                  props.documentSetName +
+                  "/" +
+                  document.documentId
+                }
+              >
+                {document.documentId}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <DocumentUploadForm documentSet={props.documentSetName} />
+      </div>
+    );
+  }
 }
