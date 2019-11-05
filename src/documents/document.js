96603c849aa3138d7c7e1f184793608c63c50582
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2019-11-05 11:01:57 +0000

    feat: sensitvity prediction

--- src/documents/document.js
+++ src/documents/document.js
@@ -4,9 +4,11 @@ import { DocumentApi } from "@harpocrates/api-client";
 
 export default function Document(props) {
   const [document, setDocument] = useState([]);
+  const [predictedClassification, setPredictedClassification] = useState(null);
 
   var api = new DocumentApi();
 
+  // effect for getting document
   useEffect(
     () => {
       api
@@ -17,12 +19,32 @@ export default function Document(props) {
     },
     [] //dependencies
   );
-  // tell if no document Sets
 
+  api.predictedClassification;
+  // effect for getting document classification
+  useEffect(
+    () => {
+      api
+        .getPredictedClassification(props.documentSetName, props.documentId)
+        .then(apiPredictedClassification => {
+          setPredictedClassification(apiPredictedClassification);
+        });
+    },
+    [] //dependencies
+  );
   return (
     <div>
       <h1>{document.name}</h1>
       <h2>{document.documentId}</h2>
+      <h3>
+        {predictedClassification
+          ? `Prediction: ${
+              predictedClassification.sensitive
+                ? "Sensitive Document"
+                : "Non-sensitive document"
+            } (${predictedClassification.sensitivity}% sensitive)`
+          : "getting classification..."}
+      </h3>
       <p>{document.content}</p>
     </div>
   );
