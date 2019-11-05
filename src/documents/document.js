2e89cb2266158da2d47e68b86f2983f0eaa5a28f
guillaumedsde <guillaume@desusanne.com>
2019-11-05 22:25:28 +0000

    feat: material menu and document sensitivity view

--- src/documents/document.js
+++ src/documents/document.js
@@ -1,42 +1,49 @@
 import React, { useEffect, useState } from "react";
 
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Box from "@material-ui/core/Box";
+
 import { DocumentApi } from "@harpocrates/api-client";
-import Highlighter from "react-highlight-words";
 
 import PredictedClassification from "./documentPredictedClassification";
+import DocumentBody from "./documentBody";
 
 export default function Document(props) {
-  const [document, setDocument] = useState([]);
+  const [document, setDocument] = useState(null);
 
   var api = new DocumentApi();
 
   // effect for getting document
-  useEffect(
-    () => {
-      api
-        .getDocument(props.documentSetName, props.documentId)
-        .then(apiDocument => {
-          setDocument(apiDocument);
-        });
-    },
-    [] //dependencies
-  );
-
-  return (
-    <div>
-      <h1>{document.name}</h1>
-      <h2>{document.documentId}</h2>
-      <PredictedClassification
-        documentSet={props.documentSetName}
-        documentId={props.documentId}
-      />
-      <Highlighter
-        highlightClassName="YourHighlightClass"
-        searchWords={["and", "or", "the"]}
-        autoEscape={true}
-        textToHighlight={document.content}
-      />
-      ,
-    </div>
-  );
+  useEffect(() => {
+    api
+      .getDocument(props.documentSetName, props.documentId)
+      .then(apiDocument => {
+        setDocument(apiDocument);
+      });
+  }, []);
+  if (document) {
+    return (
+      <div>
+        <h1>{document.name}</h1>
+        <h2>{document.documentId}</h2>
+        <Box my={2}>
+          <PredictedClassification
+            documentSet={props.documentSetName}
+            documentId={props.documentId}
+          />
+        </Box>
+        <DocumentBody
+          documentSetName={props.documentSetName}
+          documentId={props.documentId}
+          documentContent={document.content}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <CircularProgress />
+      </div>
+    );
+  }
 }
