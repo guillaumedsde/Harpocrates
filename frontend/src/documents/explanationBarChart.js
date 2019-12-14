f230abe6c7119c423fcbf70e5c18178d31504d7e
guillaumedsde <guillaume@desusanne.com>
2019-12-14 12:28:00 +0100

    feat: explanation chart page layout

--- frontend/src/documents/explanationBarChart.js
+++ frontend/src/documents/explanationBarChart.js
@@ -8,40 +8,43 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ReferenceLine
+  ReferenceLine,
+  ResponsiveContainer
 } from "recharts";
 import { CircularProgress } from "@material-ui/core";
 
 export default function ExplanationChart(props) {
   if (props.explanationFeatures) {
     return (
-      <BarChart
-        width={500}
-        height={300}
-        data={props.explanationFeatures}
-        layout="vertical"
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <YAxis dataKey="text" type="category" />
-        <XAxis type="number" />
-        <Tooltip />
-        <ReferenceLine x={0} stroke="#000" />
-        {/* Color depending on positivity of value */}
-        <Bar dataKey="weight">
-          {props.explanationFeatures.map((entry, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={entry.weight > 0 ? "#2ca02c" : "#d62728"}
-            />
-          ))}
-        </Bar>
-      </BarChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={0}
+          height={0}
+          data={props.explanationFeatures}
+          layout="vertical"
+          margin={{
+            top: 5,
+            right: 30,
+            left: 30,
+            bottom: 5
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <YAxis dataKey="text" type="category" />
+          <XAxis type="number" />
+          <Tooltip />
+          <ReferenceLine x={0} stroke="#000" />
+          {/* Color depending on positivity of value */}
+          <Bar dataKey="weight">
+            {props.explanationFeatures.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={entry.weight > 0 ? "#2ca02c" : "#d62728"}
+              />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
     );
   } else {
     return <CircularProgress />;
