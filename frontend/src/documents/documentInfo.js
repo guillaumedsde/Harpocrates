816e6a60bbefad25cc2576666b6a2bd8a0d5add9
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2019-12-16 14:12:25 +0100

    feat: cleanup in frontend

--- frontend/src/documents/documentInfo.js
+++ frontend/src/documents/documentInfo.js
@@ -1,19 +1,18 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 
-import PredictedClassification from "./documentPredictedClassification";
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 export default function DocumentInfo(props) {
   return (
-    <Grid container alignItems="center">
+    <Grid container>
       {props.document.name ? (
         <Grid item xs>
           <Typography variant="h4">{props.document.name}</Typography>
         </Grid>
       ) : null}
       <Grid item xs>
-        <Typography variant="h5">{props.document.documentId}</Typography>
+        <Typography variant="h6">{props.document.documentId}</Typography>
       </Grid>
     </Grid>
   );
