18cc3dede8e4a980ede5983b09a80b3029e13803
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2019-12-16 09:59:02 +0100

    feat: sensitivity part of ItemList

--- frontend/src/documents/documentPredictedClassification.js
+++ frontend/src/documents/documentPredictedClassification.js
@@ -1,41 +1,32 @@
 import React from "react";
 
-import Chip from "@material-ui/core/Chip";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import WarningIcon from "@material-ui/icons/Warning";
-import { Grid, Typography, LinearProgress } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import { LinearProgress } from "@material-ui/core";
 
 export default function PredictedClassification(props) {
   return (
-    <>
-      <Grid item xs>
-        <Chip
-          style={{ margin: "auto" }}
-          label={`${
-            props.classification.sensitive
-              ? "Sensitive Document"
-              : "Non-sensitive document"
-          }`}
-          color={props.classification.sensitive ? "primary" : "default"}
-          icon={
-            props.classification.sensitive ? (
-              <WarningIcon fontSize="small" />
+    <List subheader={<ListSubheader>Sensitivity</ListSubheader>}>
+      <ListItem alignItems="flex-start">
+        <ListItemText
+          primary={`${
+            props.classification ? props.classification.sensitivity : "? "
+          }% sensitive`}
+          secondary={
+            props.classification ? (
+              <LinearProgress
+                value={props.classification.sensitivity}
+                variant="determinate"
+                color={props.classification.sensitive ? "secondary" : "primary"}
+              />
             ) : (
-              <CheckCircleIcon fontSize="small" />
+              <LinearProgress />
             )
           }
         />
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="body1">
-          {props.classification.sensitivity}% sensitive
-        </Typography>
-        <LinearProgress
-          value={props.classification.sensitivity}
-          variant="determinate"
-          color={props.classification.sensitive ? "secondary" : "primary"}
-        />
-      </Grid>
-    </>
+      </ListItem>
+    </List>
   );
 }
