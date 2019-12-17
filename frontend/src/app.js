818c2ea5c3bdb653f21a528c235446fd6d728ec5
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2019-12-17 19:45:59 +0100

    feat: dark theme toggle

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
+
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Home from "./home";
 
 import DocumentSets from "./documentSets/documentSets";
@@ -11,18 +14,34 @@ import Document from "./documents/document";
 import Dashboard from "./dashboard";
 
 const App = () => {
+  // dark mode
+  const [darkTheme, setDarkTheme] = useState(
+    useMediaQuery("(prefers-color-scheme: dark)")
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: darkTheme ? "dark" : "light"
+        }
+      }),
+    [darkTheme, setDarkTheme]
+  );
   return (
     // does nothing in production
     // but gives additional warnings in development
     // <React.StrictMode>
-    <Dashboard>
-      <Router>
-        <Home path="/" />
-        <DocumentSets path="documentSets" />
-        <DocumentSet path="documentSets/:documentSetName" />
-        <Document path="documentSets/:documentSetName/:documentId" />
-      </Router>
-    </Dashboard>
+    <ThemeProvider theme={theme}>
+      <Dashboard darkTheme={darkTheme} setDarkTheme={setDarkTheme}>
+        <Router>
+          <Home path="/" />
+          <DocumentSets path="documentSets" />
+          <DocumentSet path="documentSets/:documentSetName" />
+          <Document path="documentSets/:documentSetName/:documentId" />
+        </Router>
+      </Dashboard>
+    </ThemeProvider>
     // </React.StrictMode>
   );
 };
