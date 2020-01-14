687e6a5d1b891d0eb127c487efefe0095a467ea8
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2020-01-14 17:03:18 +0000

    fix: center breadcrumbs and set max length to 4

--- frontend/src/navigation/breadcrumbsNavigation.js
+++ frontend/src/navigation/breadcrumbsNavigation.js
@@ -3,6 +3,7 @@ import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import HomeIcon from "@material-ui/icons/Home";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,6 +12,9 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     padding: theme.spacing(1, 2)
+  },
+  link: {
+    display: "flex"
   }
 }));
 
@@ -19,18 +23,43 @@ export default function BreadcrumbsNavigation(props) {
 
   const urlFragments = props.location.pathname.split("/");
   var locations = [];
+
+  locations.push({
+    path: "/",
+    location: (
+      <>
+        <HomeIcon />
+        Home
+      </>
+    )
+  });
   var path = "";
   urlFragments.forEach(location => {
-    path = path.concat(location).concat("/");
-    const locationObj = { path: path, location: location };
-    locations.push(locationObj);
+    if (location) {
+      path = path.concat(location).concat("/");
+      const locationObj = { path: path, location: location };
+      locations.push(locationObj);
+    }
   });
 
   return (
-    <Paper elevation={0} className={classes.paper}>
-      <Breadcrumbs aria-label="breadcrumb">
+    <Paper elevation={0} className={classes.paper} style={{ maxWidth: "100%" }}>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        maxItems={4}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         {locations.map(location => (
-          <Link key={location} color="inherit" href={location.path}>
+          <Link
+            key={location}
+            color="inherit"
+            href={location.path}
+            className={classes.link}
+          >
             {location.location}
           </Link>
         ))}
