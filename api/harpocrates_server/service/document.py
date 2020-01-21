d294608bf46650ca1cede548aac2abd13c4ff57c
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2020-01-21 20:45:18 +0000

    WIP

--- api/harpocrates_server/service/document.py
+++ api/harpocrates_server/service/document.py
@@ -1,6 +1,8 @@
 from typing import Dict
-from harpocrates_server.models.document import Document
 from copy import deepcopy
+
+from harpocrates_server.models.document import Document
+from openapi_server.models.paragraph import Paragraph
 
 
 def document_from_mongo_dict(doc: Dict) -> Document:
@@ -9,3 +11,18 @@ def document_from_mongo_dict(doc: Dict) -> Document:
     del document_dict["_id"]
     document = Document().from_dict(document_dict)
     return document
+
+
+def paragraphs_from_content(content: str) -> List[Paragraph]:
+    """
+    Given a string representation of document content,
+    this function splits it into a list of Paragraph objects 
+
+    :param content: document content to split
+    """
+    delimiter = "\n"
+    paragraphs_content = content.splitlines(True)
+    paragraphs = []
+    for paragraph_content in paragraphs_content:
+        paragraphs.append(Paragraph(content=paragraph_content))
+    return paragraphs
