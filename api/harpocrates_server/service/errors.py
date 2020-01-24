74552dce5e7a84cf5421b50babb6536aa0c6788b
guillaumedsde <guillaume@desusanne.com>
2020-01-24 17:34:03 +0000

    fix: syntax error

--- api/harpocrates_server/service/errors.py
+++ api/harpocrates_server/service/errors.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
 from harpocrates_server.models.http_status import HttpStatus as ApiHttpStatus
 
-def create_api_http_status(error: HTTPStatus): -> ApiHttpStatus
+
+def create_api_http_status(error: HTTPStatus) -> ApiHttpStatus:
     return ApiHttpStatus(
-            code=error.value,
-            message="{phrase} : {description}".format(
-                phrase=error.phrase, description=error.description
-            ),
-        )
+        code=error.value,
+        message="{phrase} : {description}".format(
+            phrase=error.phrase, description=error.description
+        ),
+    )
+
