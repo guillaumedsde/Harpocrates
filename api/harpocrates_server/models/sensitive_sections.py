b98b763ebb132a1bd028183ba8479cbe18aa4c43
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2020-01-27 17:13:09 +0000

    fix: camelCase

--- api/harpocrates_server/models/sensitive_sections.py
+++ api/harpocrates_server/models/sensitive_sections.py
@@ -5,7 +5,7 @@ from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
 
-from harpocrates_server.models.base_model_ import Model
+from harpocrates_server.models.base_model import Model
 from harpocrates_server.models.sensitive_section import SensitiveSection
 from harpocrates_server import util
 
