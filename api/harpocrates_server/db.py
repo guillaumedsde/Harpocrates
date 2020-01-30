9082473af26a318f585ffcdeedd9a3c3d4290378
guillaumedsde <guillaume@desusanne.com>
2020-01-30 11:46:05 +0000

    fix: use document_sets mongo collection name

--- api/harpocrates_server/db.py
+++ api/harpocrates_server/db.py
@@ -6,5 +6,5 @@ from pymongo import MongoClient
 def create_db_client():
     MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://root:example@localhost:27017/"
     mongo = MongoClient(MONGO_URI)
-    db = mongo["document_sets_text_content_level"]
+    db = mongo["document_sets"]
     return db
