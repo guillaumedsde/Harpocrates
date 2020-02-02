8c180a2592e4ea4c534cbb2d3f884d01e81c6466
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2020-02-02 18:13:55 +0000

    fix: saved ThunderSVM classifier

--- api/harpocrates_server/service/classification.py
+++ api/harpocrates_server/service/classification.py
@@ -105,9 +105,20 @@ def train(classifier, skip_training=False):
 
 def train_and_store_classifier(classifier, path):
     classifier_type = type(classifier).__name__
-    print("training model for %s, and storing it at %s" % (classifier_type, path))
+    print("training model for %s, and storing it at" % (classifier_type))
     trained_classifier = train(classifier)
-    # dump(trained_classifier, path)
+
+
+    path.mkdir(parents=True, exist_ok=True)
+
+    print("storing trained classifier at {}".format(path))
+    trained_classifier.named_steps.clf.save_to_file(str(path.joinpath("clf")))
+    print("stored trained classifier at {}".format(path))
+
+    print("storing fitted vectorizer at {}".format(path))
+    dump(trained_classifier.named_steps.vect, str(path.joinpath("vect")))
+
+    print("stored fitted vectorizer at {}".format(path))
     return trained_classifier
 
 
@@ -118,13 +129,13 @@ def get_model(classifier=None, retrain=False):
         classifier = CLASSIFIERS[0]
 
     classifier_type = type(classifier).__name__
-    model_path = MODELS_DIRECTORY.joinpath(classifier_type + ".joblib")
+    model_path = MODELS_DIRECTORY.joinpath(classifier_type)
     try:
         global_model_name = type(MODEL[-1]).__name__
     except NameError:
         global_model_name = None
 
-    if retrain or not os.path.exists(model_path):
+    if retrain or not os.path.exists(model_path) or not os.listdir(model_path):
         if not os.path.exists(MODELS_DIRECTORY):
             os.makedirs(MODELS_DIRECTORY)
         trained_classifier = train_and_store_classifier(classifier, model_path)
@@ -133,6 +144,16 @@ def get_model(classifier=None, retrain=False):
         trained_classifier = MODEL
     else:
         print("loading found model for %s from %s" % (classifier_type, model_path))
-        trained_classifier = load(model_path)
+        # trained_classifier = load(model_path)
+
+
+        clf = SVC().load_from_file(str(model_path.joinpath("clf")))
+
+        print(clf)
+        vect = load(str(model_path.joinpath("vect")))
+
+        trained_classifier = Pipeline(
+            steps=[("vect", vect), ("clf", clf),], verbose=True,
+        )
         MODEL = trained_classifier
     return trained_classifier, classifier_type
